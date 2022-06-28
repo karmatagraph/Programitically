8d0266483caf6b9aeb63c39a0d2f1a47cb6113ee
karma <karmatashi954@gmail.com>
2022-06-28 09:27:41 +0545

    Initial Commit

---
+++ Programitically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programitically
+//
+//  Created by karma on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
